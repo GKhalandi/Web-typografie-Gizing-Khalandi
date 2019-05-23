7b7c40873644fd00b9ce897e3b5a36b2a823123a
Gizing Khalandi <gzing20@me.com>
2019-05-23 09:15:49 +0200

    online zetten

---
+++ captions.js
@@ -0,0 +1,65 @@
+// Master Windu. I take it General Grievous has been destroyed, then.
+captions[0] = ['1.1','5.513'];
+captions[1] = ['6.48','8.888'];
+captions[2] = ['9.025','12.607'];
+
+// you're under arrest, Chancellor.
+captions[3] = ['12.737','14.646'];
+captions[4] = ['14.78','17.354'];
+captions[5] = ['17.491','19.4'];
+
+// - I AM the senate.
+captions[6] = ['19.535','23.367'];
+captions[7] = ['28.72','31.036'];
+captions[8] = ['32.619','34.62'];
+
+// You are under arrest, my lord.
+captions[9] = ['130.02','133.56'];
+captions[10] = ['134.69','138.811'];
+captions[11] = ['139.111','141.234'];
+
+// The oppression of the Sith will never return.
+captions[12] = ['141.364','143.059'];
+captions[13] = ['143.94','146.14'];
+captions[14] = ['147.06','151.94'];
+
+// He's a traitor!
+captions[15] = ['154.71','156.584'];
+captions[16] = ['156.72','160.58'];
+captions[17] = ['162.04','166.2'];
+
+// You must choose.
+captions[18] = ['167.222','169.131'];
+captions[19] = ['170.22','173.38'];
+captions[20] = ['173.9','176.52'];
+
+// I can't hold it any longer.
+captions[21] = ['179.66','181.36'];
+captions[22] = ['182.46','183.78'];
+captions[23] = ['184.357','186.464'];
+// Anakin!
+captions[24] = ['187','188.330'];
+captions[25] = ['189.32','190.54'];
+captions[26] = ['194.98','198.66'];
+// I am going to end this once and for all.
+captions[27] = ['199','202.377'];
+captions[28] = ['202.507','203.965'];
+captions[29] = ['204.092','206.049'];
+// He has control of the senate and the courts.
+captions[30] = ['207.095','209.02'];
+captions[31] = ['209.639','211.545'];
+captions[32] = ['212.3','213.08'];
+// - Oh. Don't kill me. Please.
+captions[33] = ['214.98','217.94'];
+captions[34] = ['218.6','220.1'];
+captions[35] = ['222.7','223.24'];
+// Please don't!
+captions[36] = ['224.28','225.52'];
+captions[37] = ['226.54','227.54'];
+captions[38] = ['227.63','228.6'];
+// POWER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+captions[39] = ['229.68','232.83'];
+captions[40] = ['234.38','236.54'];
+captions[41] = ['243.96','245.26'];
+// POWER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+captions[42] = ['246.26','250'];
---
+++ sounds.js
@@ -0,0 +1,12 @@
+// Hier kan je tijden toevoegen in secondes, geluiden in closed captions
+// Op die mommenten komt er een class op de body.
+sounds = [
+	128,
+	151,
+	192,
+	193,
+	194,
+	225,
+	228,
+	245.5
+];
